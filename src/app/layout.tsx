c1d79390b5e1ea551b035d06b9b7e82d34b5408f
Jatin Saini <jatin4762@gmail.com>
2024-01-21 16:19:14 +0530

    Auth working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Navbar from "@/components/nav/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={cn("min-h-screen font-sans antialiased grainy", inter.className)}
       >
+        <Navbar />
         {children}
       </body>
     </html>
